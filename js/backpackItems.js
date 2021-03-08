2c4f525c5559a0611c67b34ef01ed4913867cfe9
MAbotter <botter.marco@gmail.com>
2021-03-08 12:05:57 +0100

    Implementata funzione che fornisce oggett random; Aggiunti altri oggetti di test; Passaggi livello di progetto a ES6; Creata pagina html per il interno test in console.

--- js/backpackItems.js
+++ js/backpackItems.js
@@ -2,7 +2,7 @@
  * Liberty Software
  * Progetto     : Luccio
  * File         : backpackItems.jd
- * Descrizione  : Array degli oggetti preseni nel gioco
+ * Descrizione  : Array degli oggetti presenti nel gioco e funzioni relative
  * Autore       : Marco Botter
  * Data         : 07/03/2021
  * Versione     : Alpha
@@ -15,7 +15,7 @@ let backpackItems=[
         abundance: 10,                              //Abbondanza
         type: "food",                               //Tipo (se non serve scambiarlo non serve)
         perform: function (char){                   //Funzione all'uso dell'oggetto (char è il personaggio che lo usa)
-            char.hunger--;                          //Riduci la fame
+            char.feed(1);                           //Riduci la fame
         },
         icon: ""                                    //File dell'icona
     },
@@ -28,5 +28,43 @@ let backpackItems=[
             char.enemy.damage(1);                   //Fai un danno al nemico
         },
         icon: ""
+    },
+    {
+        name: "Libro",
+        description: "\"A piedi nudi su Marte\", di Adrian Fartade",
+        abundance: 20,
+        type: "mood",
+        perform: function (char){
+            char.enjoy(1);                          //Migliora l'umore leggendolo
+        },
+        icon: ""
+    },
+    {
+        name: "Garza",
+        description: "Un veloce medicamento",
+        abundance: 5,
+        type: "medicament",
+        perform: function (char){
+            char.heal(1);                           //Ti curi un po con la garza
+        },
+        icon: ""
+    },
+    {
+        name: "Motosega",
+        description: "BWA-AH_AH Siete tutti morti!",
+        abundance: 1,
+        type: "weapon",
+        perform: function (char){
+            char.enemy.damage(4);                   //Fai un sacco di danni al nemico
+        },
+        icon: ""
     }
-]
+];
+
+/*
+ * Ritorna un'oggetto a caso tra quelli presenti
+ * TODO: Implementare un correttivo per l'abondanza nei piani
+ */
+function getRandomObject()
+{   return(backpackItems[Math.floor(Math.random() * (backpackItems.length ))]); //Ritorna oggetto casuale
+}
--- js/characters.js
+++ js/characters.js
@@ -15,29 +15,48 @@
 /*
  * Costanti
  */
-const INITIAL_HEALTH=4;                 //Salute iniziale di base
-const INITIAL_HUNGER=0;                 //Fame iniziale di base
-const INITIAL_MOOD=4;                   //Umore iniziale di base
+const INITIAL_HEALTH=4;                             //Salute iniziale di base
+const INITIAL_HUNGER=0;                             //Fame iniziale di base
+const INITIAL_MOOD=4;                               //Umore iniziale di base
+const HUNGER_MIN=0;                                 //Fame minima
+const HEALTH_MIN=0;                                 //Salute minima
+const MOOD_MIN=0;                                   //Umore minimo
 
 /*
  * Costruttore personaggio
  */
 function Character(charName, charImage, pastBag)
 {
-    this.name=charName;                 //Nome del personaggio
-    this.image=charImage;               //Immagine del perosnaggio
-    this.bag=pastBag;                   //Oggetto nello zaiono (passato da prima, raccolto o iniziale)
-    this.health = INITIAL_HEALTH;       //Inizializza salute
-    this.hunger = INITIAL_HUNGER;       //Inizializza fame
-    this.mood = INITIAL_MOOD;           //Inizializza umore
-    this.enemy;                         //Puntatore al nemico corrente (se il personaggio è un nemico, il puntatore punta al giocatore)
-    this.setEnemy=function(char)        //Imposta nemico corrente
+    this.name=charName;                             //Nome del personaggio
+    this.image=charImage;                           //Immagine del perosnaggio
+    this.bag=pastBag;                               //Oggetto nello zaiono (passato da prima, raccolto o iniziale)
+    this.health = INITIAL_HEALTH;                   //Inizializza salute
+    this.hunger = INITIAL_HUNGER;                   //Inizializza fame
+    this.mood = INITIAL_MOOD;                       //Inizializza umore
+    this.enemy;                                     //Puntatore al nemico corrente (se il personaggio è un nemico, il puntatore punta al giocatore)
+    this.setEnemy=function(char)                    //Imposta nemico corrente
     {
-        this.enemy=char;                //Imposta il personaggio passato come nemico
-    }
-    this.damage=function(value)         //Subisci danni
+        this.enemy=char;                            //Imposta il personaggio passato come nemico
+    };
+    this.damage=function(value)                     //Subisci danni
     {
-        this.health=-value;             //Riduci salute del valore passato
-    }
+        //TODO: Controlla max e min
+        this.health=-value;                         //Riduci salute del valore passato
+    };
+    this.heal=function(value)                       //Il personaggio guarisce
+    {
+        //TODO: Controlla max e min
+        this.health+=value;
+    };
+    this.feed=function(value)                       //Il personaggio si nutre
+    {
+        //TODO; Controlla max e min
+        this.hunger-=value;
+    };
+    this.enjoy=function(value)                      //Il personaggio miegliora l'umore
+    {
+        //TODO: Controlla max e min
+        this.mood+=value;
+    };
 }
 
