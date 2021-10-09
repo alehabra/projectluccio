f9c7577bc37f293c798970d811d9051907d5bd4c
skybassettoa <alessandro.bassetto.nttdata@skytv.it>
2021-10-09 19:20:57 +0200

    grafica dialogo

---
+++ ui/js/dialogs.js
@@ -0,0 +1,9 @@
+var dialog = {
+    "giorno1": {
+      "inizio": [
+        {"value": "New", "onclick": "CreateNewDoc()"},
+        {"value": "Open", "onclick": "OpenDoc()"},
+        {"value": "Close", "onclick": "CloseDoc()"}
+      ]
+    }
+  };
---
+++ ui/js/main.js
@@ -0,0 +1,5 @@
+////////////TRIGGER DIALOGO//////////////
+let dialogActive = document.querySelectorAll('.dialog--active');
+let IsdialogActive = dialogActive[0];
+
+console.log(dialog);
