8d2199af016c44d18b04b0ed56fc746b24b38701
skybassettoa <alessandro.bassetto.nttdata@skytv.it>
2021-10-09 20:10:07 +0200

    fix per safari?

--- ui/js/dialogs.js
+++ ui/js/dialogs.js
@@ -1,9 +1,21 @@
-var dialog = {
-    "giorno1": {
+var dialogJSON = {
+    "uno": {
       "inizio": [
-        {"value": "New", "onclick": "CreateNewDoc()"},
-        {"value": "Open", "onclick": "OpenDoc()"},
-        {"value": "Close", "onclick": "CloseDoc()"}
+        {"char": "main","line":"dove diavolo sono?"},
+        {"char": "main","line":"non pensavo che il dentista avesse uno studio così di merda..."},
+        {"char": "npc","line":"non sei dal dentista amico..."},
+        {"char": "main","line":"chi parla? l'assistente?"},
+        {"char": "main","line":"non hai una voce femminile"},
+        {"char": "npc","line":"non fare il sessista"},
       ]
-    }
+    },
+    "due": {
+        "inizio": [
+          {"char": "main","line":"che.. cazzo.."},
+          {"char": "main","line":"...mi hanno addormentato?"},
+          {"char": "npc","line":"...facci il callo bellezza..."},
+          {"char": "npc","line":"...ogni giorno sarà così..."},
+          {"char": "npc","line":"...io la prendo con filosofia..."},
+        ]
+      }
   };
--- ui/js/main.js
+++ ui/js/main.js
@@ -1,5 +1,17 @@
 ////////////TRIGGER DIALOGO//////////////
+const dialog = document.getElementById('dialogs')
 let dialogActive = document.querySelectorAll('.dialog--active');
 let IsdialogActive = dialogActive[0];
 
-console.log(dialog);
+function StartDialog(type,day){
+    dialog.classList.add('dialogs--active')
+    if(dialogJSON.uno.inizio[0].char == 'main'){
+
+    }
+}
+
+function ContinueDialog(type,day,lineNum){
+
+}
+
+console.log(dialogJSON);
