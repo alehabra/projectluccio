366ccac9a1069e593e5f28a6840b2abec6cb24ca
skybassettoa <alessandro.bassetto.nttdata@skytv.it>
2021-10-17 19:56:34 +0200

    iterazione oggetti

--- ui/js/main.js
+++ ui/js/main.js
@@ -2,7 +2,6 @@
 const dialog = document.getElementById('dialogs');
 let dialogNum = 0;
 let IsdialogActive;
-IsdialogActive[0].onclick = ContinueDialog();
 
 function StartDialog(){
     //creo box dialogo
@@ -33,33 +32,13 @@ function StartDialog(){
     
 }
 
-//click sul dialogo attivo
-//IsdialogActive.onclick = ContinueDialog();
 
-function ContinueDialog(){
-    //rimuovo attivazione da dialogo precendente
-    let dialogActive = document.querySelectorAll('.dialog');
-    dialogActive[0].classList.remove('dialog--active');
+////////////BOX OGGETTI//////////////
+const boxOggetto = document.getElementById('objSelector');
 
-    //creo il box dialogo
-    var dialogBox = document.createElement('div');
-    var dialogBack = document.createElement('div');
-    if(dialogJSON.inizio[0][0].char == 'main'){
-        dialogBox.classList.add('dialog-char');
-        dialogBack.classList.add('dialog-char-back');
-    } else{
-        dialogBox.classList.add('dialog-npc');
-        dialogBack.classList.add('dialog-npc-back');
-    }
-    dialogBox.classList.add('dialog');
-    dialogBox.classList.add('dialog--active');
-
-    //inserisco il testo
-    dialogBox.innerHTML= dialogJSON.inizio[0][1].line;
-    dialogBox.appendChild(dialogBack);
-    dialog.appendChild(dialogBox);
-
-    //incremento la linea di dialogo
-    dialogNum++
+function showBoxObjects(){
+    boxOggetto.classList.add('objSelector--active');
 }
-
+function hideBoxObjects(){
+    boxOggetto.classList.remove('objSelector--active');
+}
